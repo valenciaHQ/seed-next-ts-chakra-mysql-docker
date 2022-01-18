964c1a32817d62b416872e374ce39da654449618
Alejandro Valencia <alejandro.d.valencia@gmail.com>
2022-01-18 16:39:13 -0300

    added chakra-UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import { Box, Center, Container } from "@chakra-ui/react";
+
 export default function IndexPage() {
-  return <div>Hello world</div>;
+  return (
+    <Container
+      maxW="xl"
+      height="100vh"
+      centerContent
+      display="flex"
+      justifyContent="center"
+    >
+      <Center bg="tomato" w="xl" h="100px" color="white">
+        This is the Center
+      </Center>
+    </Container>
+  );
 }
