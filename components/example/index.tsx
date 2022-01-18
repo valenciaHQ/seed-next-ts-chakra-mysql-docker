7a7bce413cd00f25127fbbdd0f8197b3bdeb77b9
Alejandro Valencia <alejandro.d.valencia@gmail.com>
2022-01-18 16:26:08 -0300

    updated gitignore

--- components/example/index.tsx
+++
@@ -1,5 +0,0 @@
-function Button({ children }) {
-  return <button>{children}</button>;
-}
-
-export default Button;
--- lib/db.ts
+++
@@ -1,9 +0,0 @@
-// get the client
-import mysql from "mysql2/promise";
-
-export const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  database: process.env.MYSQL_DATABASE,
-  password: process.env.MYSQL_PASSWORD,
-});
